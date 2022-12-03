eab29acac9abd4f464e8a3d570829add94c5f8d8
stylianosvaf <118131476+stylianosvaf@users.noreply.github.com>
2022-12-03 14:43:39 -0500

    Update SERVOJOYSTICK.ino

--- SERVOJOYSTICK.ino
+++ SERVOJOYSTICK.ino
@@ -2,11 +2,11 @@
 
 #include <Servo.h>
 
-#define joyX      A0 // connect to x axis of the joystick
+#define joyX      A0  // connect to x axis of the joystick
 #define servoMotor  2  // connect pin 2 to the servo motor
 
 
-Servo servoX;  // link the library to the servo
+Servo servoX;  // create a servo object
 
 void setup() {
   Serial.begin(9600) ;
@@ -15,8 +15,8 @@ void setup() {
 
 void loop() {
   
-  int valueX = analogRead(joyX); // read the joystick's value
+  int valueX = analogRead(joyX);  // read the joystick's value
 
-  int angleX = map(valueX, 0, 1023, 0, 180); // change it to have the range of the servo
+  int angleX = map(valueX, 0, 1023, 0, 180);  // change it to have the range of the servo
 
-  servoX.write(angleX); // move the servo depending on the scaled value of the joystick
+  servoX.write(angleX);  // move the servo depending on the scaled value of the joystick
