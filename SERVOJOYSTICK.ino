0b08571c9c275bb13512c6b433f65e373a72252b
stylianosvaf <118131476+stylianosvaf@users.noreply.github.com>
2022-12-03 14:00:12 -0500

    Add files via upload

---
+++ SERVOJOYSTICK.ino
@@ -0,0 +1,22 @@
+/* Moving a servo motor using a joystick */
+
+#include <Servo.h>
+
+#define joyX      A0 // connect to x axis of the joystick
+#define servoMotor  2  // connect pin 2 to the servo motor
+
+
+Servo servoX;  // link the library to the servo
+
+void setup() {
+  Serial.begin(9600) ;
+  servoX.attach(servoMotor);  // attach a servo to the pin
+}
+
+void loop() {
+  
+  int valueX = analogRead(joyX); // read the joystick's value
+
+  int angleX = map(valueX, 0, 1023, 0, 180); // change it to have the range of the servo
+
+  servoX.write(angleX); // move the servo depending on the scaled value of the joystick
